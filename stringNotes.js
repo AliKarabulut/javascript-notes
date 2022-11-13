861f66731780b2fdd9d22bac18463dbaf148b8bb
Ali Karabulut <a.kbt6666@gmail.com>
2022-11-13 23:43:02 +0300

    at()

---
+++ stringNotes.js
@@ -0,0 +1,10 @@
+//string.at
+
+// at bir tam sayı değeri alır ve stringte karşılık gelen değeri dönderir
+
+const at = 'Kediler köpeklerden daha zekidir.'
+
+let index = -5
+
+console.log(at.at(index))
+
