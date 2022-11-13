fd476bb46846e15873c31ef2a277a5cab30febfb
Ali Karabulut <a.kbt6666@gmail.com>
2022-11-13 23:59:21 +0300

    charAt()

--- stringNotes.js
+++ stringNotes.js
@@ -1,11 +1,11 @@
-//string.at
-
-// at bir tam sayı değeri alır ve stringte karşılık gelen değeri dönderir
-
 const at = 'Kediler köpeklerden daha zekidir.'
+let index = 5
 
-let index = -5
+//at()
+// at bir tam sayı değeri alır ve stringte karşılık gelen değeri dönderir
+// console.log(at.at(index))
 
-console.log(at.at(index))
-
+//charAt()
+//Eksi değer almaz!!
+// console.log(at.charAt(index))
 
